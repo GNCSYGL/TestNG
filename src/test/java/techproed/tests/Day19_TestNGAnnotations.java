84499a1c30170786cf7d8c57b1b5720d897aa3ee
Gonca Saygili <goncasaygili@gmail.com>
2023-01-21 23:54:23 +0300

    TestNG-01

---
+++ src/test/java/techproed/tests/Day19_TestNGAnnotations.java
@@ -0,0 +1,90 @@
+package techproed.tests;
+
+import org.testng.annotations.*;
+
+public class Day19_TestNGAnnotations {
+    /*
+    @Test: Test case olusturmak icin kullanilir.
+    @Before ve @After: 5 Before, 5 After anotations var. Faydasi: testi calistirdigimizda testin seviyelerini siralar
+    suite > tests > groups > class > method
+    @BeforeSuite: her bir test suite'ten once 1 sefer calisir.
+    @AfterSuite : her bir test suite'ten sonra 1 sefer calisir.
+    @BeforeTest: her bir testten once (TEST CASE ILE KARISTIRILMAMALI) 1 sefer calisir
+    @AfterTest: her bir testten sonra (TEST CASE ILE KARISTIRILMAMALI) 1 sefer calisir
+    @BeforeClass: her bir class'tan once 1 sefer calisir.
+    @AfterClass: her bir class'tan sonra 1 sefer calisir.
+    @Before Method : her bir @Test annotationdan once tek sefer calisir. JUnitdeki karsiligi sadece @Before
+    @After Method : her bir @Test annotationdan sonra tek sefer calisir. JUnitdeki karsiligi sadece @After
+    ----------------------------------------------------------------------------------------------------------
+    @Ignore: @Test case'leri calistirmak istemedigimizde atlamak (skip,ignore) icin kullanilir.
+    (enabled = false): @Test case'leri kullanima kapatmak icin kullanilir. @Ignore'dan daha guclu bir yapisi vardir. Hata riskini sifir yapar.
+    ----------------------------------------------------------------------------------------------------------
+    NOT: TestNG'de testler alfabetik siraya gore (national order) calisir. Yukaridan asagiya calismaz.
+    @Test(priority = 1): Test case'leri oncelemek icin kullanilir.
+    "@Test" olarak olusturdugumuz testlerin prioroty'si aslinda 0'dir. Default deger olarak gorunmez sekilde durur, yazilmaz.
+    Ornek: @Test = @Test(priority = 0)
+     */
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("Before Suite");
+    }
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("Before Test");
+    }
+    @BeforeGroups
+    public void beforeGroups(){
+        System.out.println("Before Groups");
+    }
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("Before Class");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("Before Method");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("After Suite");
+    }
+    @AfterTest
+    public void afterTest(){
+        System.out.println("After Test");
+    }
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("After Class");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("After Method");
+    }
+    @Test(priority = 1)
+    public void test7(){
+        System.out.println("Test 7");
+    }
+    @Test @Ignore
+    public void test2(){
+        System.out.println("Test 2");
+    }
+    @Test(enabled = false)
+    public void test3(){
+        System.out.println("Test 3");
+    }
+    @Test(priority = 4)
+    public void test4(){
+        System.out.println("Test 4");
+    }
+    @Test(priority = 2)
+    public void test5(){
+        System.out.println("Test 5");
+    }
+    @Test(priority = -5)
+    public void test6(){
+        System.out.println("Test 6");
+    }
+//    test6 >test7 >test5 > test4
+
+}
